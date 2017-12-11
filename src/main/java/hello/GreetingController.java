016c9ed441ef09351318eeed2ddf0bf6128e81d2
Dahlia453 <Dahlia453@Dahlia453.bpkp.go.id>
2017-12-11 12:24:55 +0700

    first commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,12 @@
 package hello;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -36,5 +43,36 @@ public class GreetingController {
     			 	line.contains(prefix))
     			 .collect(Collectors.toList());
           
-    }
+    	     }
+     
+     @RequestMapping("countries")
+     public String getCountries() throws IOException{
+     	URL url = new URL("http://www.webservicex.net/country.asmx/GetCountries");
+
+     URLConnection connection = url.openConnection();
+     connection.setDoOutput(true);
+     connection.setRequestProperty("Content-Type", "$application/x-www-form-urlenconded");
+     connection.setRequestProperty("Content-Length","0");
+     
+     InputStream stream = connection.getInputStream();
+     
+     /* return new BufferedReader(new InputStreamReader(stream)).
+    		 lines().collect(Collectors.joining("\n")); */
+     
+     InputStreamReader reader = new InputStreamReader(stream);
+     BufferedReader buffer = new BufferedReader(reader);
+     
+     String line;
+     StringBuilder builder = new StringBuilder();
+     while ((line = buffer.readLine())!= null) {
+    	 builder.append(line);
+     }
+     return builder.toString();
+    
+     return new BufferedReader(
+    		 new InputStreamReader(stream)).
+    		 lines().
+    		 collect(Collectors.joining("\n"));
+}
+     
 }
